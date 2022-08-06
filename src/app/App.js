631ca98bb4b429cbe91948191f3f612d3cb180f9
kikis <kikiondo117@gmail.com>
2022-08-05 21:53:07 -0500

    Grid refactor code

--- src/app/App.js
+++ src/app/App.js
@@ -4,9 +4,9 @@ import { Grid } from "../components/";
 function App() {
   return (
     <div className="bg-green-500 h-screen">
-      <Grid.Mobile className="bg-red-500 ">
-        <div className="bg-blue-500 col-start-1 col-end-4">hola mundo</div>
-      </Grid.Mobile>
+      <Grid className="bg-red-500 ">
+        <div className="col-start-1 col-end-4">hola mundo</div>
+      </Grid>
     </div>
   );
 }
--- src/components/ui/grid/Grid.jsx
+++ src/components/ui/grid/Grid.jsx
@@ -1,38 +1,28 @@
 import React from "react";
 
-function Mobile({ children, className }) {
-  return (
-    <div
-      className={`w-mobile-full max-w-mobile-full  ${
-        className ? className : ""
-      }`}
-    >
-      <div className="w-mobile h-full mx-auto px-0 grid grid-cols-custom-mobile gap-4">
-        {children}
-      </div>
-    </div>
-  );
-}
-
-function Desktop({ children, className }) {
-  return (
-    <div
-      className={` w-laptop-full max-w-laptop-full ${
-        className ? className : ""
-      }`}
-    >
-      <div className="h-full mx-auto px-0 grid grid-cols-custom-desktop gap-4 w-laptop md:w-tablet sm:w-mobile">
-        {children}
-      </div>
-    </div>
-  );
-}
-
 function Grid({ children, className }) {
-  return <div></div>;
+  return (
+    <div
+      className={` 
+      w-full 
+      max-w-laptop-full bg-purple-500
+      md:max-tablet-full md:bg-red-500
+      sm:max-w-mobile sm:bg-yellow-500
+    ${className ? className : ""}`}
+    >
+      <div
+        className="
+      w-full h-full mx-auto px-0 grid 
+      grid-cols-custom-laptop
+      md:grid-cols-custom-laptop
+      sm:grid-cols-custom-mobile gap-4
+      
+      "
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
-
-Grid.Mobile = Mobile;
-Grid.Desktop = Desktop;
 
 export { Grid };
